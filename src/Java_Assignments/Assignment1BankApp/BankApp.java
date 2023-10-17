22cb1ccb556fe4ea87c523c2e6f31c5e403de325
KaurJasnoor <kaur.jasnoor017@gmail.com>
2023-10-16 21:11:38 -0400

    Bank App Simulatar Assignment Completed

--- src/Java_Assignments/Assignment1BankApp/BankApp.java
+++ src/Java_Assignments/Assignment1BankApp/BankApp.java
@@ -3,30 +3,62 @@ package Java_Assignments.Assignment1BankApp;
 import java.util.Scanner;
 
 public class BankApp {
+    Scanner scanner = new Scanner(System.in);
+    double balance = 1000;
+    int depositCount;
+    double depositAmount;
+    int withdrawCount;
+    double withdrawAmount;
     public String welcome(){
-        System.out.println("Welcome to the Banking System Simulator");
-        System.out.println("Your current balance is : $1000");
-        return null;
-    }
-    public void mainMenu(){
-        System.out.println("a. Check Account Balance");
-        System.out.println("b. Make a Deposit");
-        System.out.println("c. Make a Withdrawal");
-        System.out.println("d. Print Account Summary");
-        System.out.println("e. Exit");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter you input");
-        String input = scanner.next();
-    }
-    public static void intialBalance(int intibalance){
-        return;
-    }
-    public void checkAccountBalance(){
-        System.out.println("Your Account Balance is : $" + intialBalance());
-    }
-    private String intialBalance() {
+        System.out.println("+++++++++++++++++++++++++++");
+        System.out.println("!!! Welcome to the Banking System Simulator !!!");
+        System.out.println(" ");
+        System.out.println("Your current balance is : " + balance);
         return null;
     }
 
-
+    public void checkingBalance(){
+        System.out.println("Checking Account Balance....");
+        System.out.println(" ");
+        System.out.println("Your current available balance is : " + balance);
+    }
+    public void deposit(){
+        System.out.println("Please enter the amount to deposit: ");
+        double amount =scanner.nextDouble();
+        if (amount <= 0){
+            System.out.println("Invalid Amount!!!");
+        }
+        else{
+            depositCount++;
+            depositAmount += amount;
+            balance += amount;
+            System.out.println("Your New Balance is : " +balance);
+        }
+    }
+    public void withdrawal(){
+        System.out.println("Enter an Amount to withdraw: ");
+        double amount = scanner.nextDouble();
+        if (amount <=0){
+            System.out.println("!!! Error !!! Withdraw amount is not valid, Try Again");
+        }
+        else if (amount <= balance){
+            withdrawCount++;
+            withdrawAmount += amount;
+            balance -= amount;
+            System.out.println("Your remaining balance is : " +balance);
+        }
+        else{
+            System.out.println("Sorry !!! You do not have enough money to withdraw from this account.");
+        }
+    }
+    public void printSummary(){
+        System.out.println("Here is your Account Summary");
+        System.out.println("+++++++++++++++++++++++++++");
+        System.out.println("Current Balance: " +balance);
+        System.out.println("Total no. of deposit: " +depositCount);
+        System.out.println("Total Amount Deposited: " +depositAmount);
+        System.out.println("Total no. of Withdrawals: " +withdrawCount);
+        System.out.println("Total Amount Withdraw: " +withdrawAmount);
+        System.out.println("+++++++++++++++++++++++++++");
+    }
 }
--- src/Java_Assignments/Assignment1BankApp/MainClassBank.java
+++ src/Java_Assignments/Assignment1BankApp/MainClassBank.java
@@ -1,10 +1,46 @@
 package Java_Assignments.Assignment1BankApp;
 
+import javax.print.CancelablePrintJob;
+import java.util.Scanner;
+
 public class MainClassBank {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         BankApp bankApp = new BankApp();
-        bankApp.welcome();
-        BankApp.intialBalance(1000);
-        bankApp.mainMenu();
+        char option;
+        do {
+            System.out.println("+++++++++++++++++++++++++++");
+            System.out.println("Main Menu");
+            System.out.println(" ");
+            System.out.println("a. Check Account Balance");
+            System.out.println("b. Make a Deposit");
+            System.out.println("c. Make a Withdrawal");
+            System.out.println("d. Print Account Summary");
+            System.out.println("e. Exit");
+            System.out.println("+++++++++++++++++++++++++++");
+            System.out.println("Please choose one of the above options: ");
+            char choice = scanner.next().charAt(0);
+            if (choice == 'a'){
+                bankApp.checkingBalance();
+            }
+            if(choice == 'b'){
+                bankApp.deposit();
+            }
+            if(choice == 'c'){
+                bankApp.withdrawal();
+            }
+            if(choice == 'd'){
+                bankApp.printSummary();
+            }
+            if(choice == 'e'){
+                System.out.println("Exit");
+                break;
+            }
+            else{
+                System.out.println("Sorry!!! Wrong choice, Please try again ");
+            }
+            System.out.println("Do you want to continue? Y/N :");
+            option = scanner.next().charAt(0);
+        }while(option == 'Y');
     }
 }
